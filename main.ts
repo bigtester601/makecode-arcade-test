bcf0b36aff72f9cffd78cb60e36481caa9f0adeb
bigtester601 <61870736+bigtester601@users.noreply.github.com>
2021-04-08 12:33:57 +0300

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,6 @@
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     music.baDing.play()
-    score += 1
 })
-let score = 0
 effects.confetti.startScreenEffect()
 let mySprite = sprites.create(assets.image`cube`, SpriteKind.Player)
 controller.moveSprite(mySprite)
